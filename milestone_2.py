4ec451ae42c2f9379ead484d46eb1f278187b880
Samia-A-Mohamed <samiaamoham@gmail.com>
2023-11-23 22:34:56 +0000

    Updated milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,19 @@
 import random
-# created a list of fruits 
-word_list = ["pinapple", "mango", "passionfruit", "lemon", "peach"]
-print(word_list)
-# generate a random fruit from the word_list
-word = random.choice(word_list)
-print(word)
-# asked the user to enter a single letter 
+word_list = ["pinapple", "mango", "passionfruit", "lemon", "peach"] 
+
+def random_word():
+   word_list = ["pinapple", "mango", "passionfruit", "lemon", "peach"]
+   word = random.choice(word_list)
+   return word.upper
+random_word()
+
 guess = input("Enter a single letter:")
-# checked that the input is a single character 
+    
 length_of_input = len(guess)
-if length_of_input == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input")
+
+def user_guess():
+    if length_of_input == 1 and guess.isalpha():
+      print("Good guess!")
+    else:
+      print("Oops! That is not a valid input")
+
